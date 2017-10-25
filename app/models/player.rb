09ceadde0b189a154639921a9b0f7266c4664a87
Rahul <rahul.26.saini@gmail.com>
2017-10-25 22:59:23 +0000

    rails

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,7 @@
 class Player < ActiveRecord::Base
   before_save :default_values
   def default_values
-    if self.win_count <0 then
+    if self.win_count != nil and self.win_count <0 then
       self.win_count = 0
     end
     self.win_count ||= 0
@@ -9,6 +9,6 @@ class Player < ActiveRecord::Base
 
   has_many :player_pokemons, :dependent => :destroy
   has_many :pokemons, -> { uniq }, :through => :player_pokemons
-  
+  has_many :fights
   validates :name, presence: { message: "Player Name is required" }, uniqueness: {message: "This name is already taken." }
 end
